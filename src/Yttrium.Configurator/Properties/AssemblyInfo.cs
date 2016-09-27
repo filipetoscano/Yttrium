2210067f822d2b6db05f7bff4c4d36a0d2d5b31b
Filipe Toscano <filipe.toscano@gmail.com>
2016-09-27 12:22:37 +0200

    + Add: Initial skeleton for yconfig

---
+++ src/Yttrium.Configurator/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Assembly information
+[assembly: AssemblyTitle( "YConfig" )]
+[assembly: AssemblyDescription( "Find / replace that doesn't break files." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Yttrium" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: CLSCompliant( false )]
+[assembly: ComVisible( false )]
+[assembly: Guid( "af42e123-8360-4a54-a831-f12fe3ae545d" )]
+[assembly: NeutralResourcesLanguage( "en-GB" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
+
+/* eof */
