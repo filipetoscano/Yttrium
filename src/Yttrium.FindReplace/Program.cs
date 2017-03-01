28988a78d73af1dd81668fbdec53d68027099a53
Filipe Toscano <filipe.toscano@gmail.com>
2017-03-01 19:14:13 +0100

    + Add: Find/Replace.

---
+++ src/Yttrium.FindReplace/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Yttrium.FindReplace
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            /*
+             * 
+             */
+            if ( args.Length != 3 )
+            {
+                Console.Error.WriteLine( "err: yfr file find replace" );
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            string file = args[ 0 ];
+            string find = args[ 1 ];
+            string repl = args[ 2 ];
+
+
+
+            /*
+             * 
+             */
+            if ( File.Exists( file ) == false )
+            {
+                Console.Error.WriteLine( "err: file '{0}' does not exist", file );
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            string content;
+
+            try
+            {
+                content = File.ReadAllText( file );
+            }
+            catch ( Exception ex )
+            {
+                Console.Error.WriteLine( "err: failed to load from '{0}'.", file );
+                Console.Error.WriteLine( ex.ToString() );
+
+                Environment.ExitCode = 2;
+                return;
+            }
+
+
+            /*
+             * 
+             */
+            content = content.Replace( find, repl );
+
+
+            /*
+             * 
+             */
+            try
+            {
+                File.WriteAllText( file, content, Encoding.UTF8 );
+            }
+            catch ( Exception ex )
+            {
+                Console.Error.WriteLine( "err: failed to write to '{0}'.", file );
+                Console.Error.WriteLine( ex.ToString() );
+
+                Environment.ExitCode = 3;
+                return;
+            }
+
+
+            Environment.ExitCode = 0;
+            return;
+        }
+    }
+}
---
+++ src/Yttrium.FindReplace/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "FindReplace" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Yttrium" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "6ed2e222-8cd5-4356-b682-a7611a34bce4" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
